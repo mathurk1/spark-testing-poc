c166620afa3a94060a8a999a43509089dda2a21b
Karthik Mathur <karthikmathur@mac.lan>
2025-11-08 10:37:57 -0800

    use delta configure_spark_with_delta_pip

--- test_delta_merge.py
+++ test_delta_merge.py
@@ -9,12 +9,10 @@ from try_delta_merge import create_initial_delta_table, perform_delta_merge
 
 @pytest.fixture(scope="session")
 def spark():
-    return SparkSession.builder \
-        .appName("test_delta_merge") \
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-        .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0") \
-        .getOrCreate()
+    from delta import configure_spark_with_delta_pip
+    builder = SparkSession.builder.appName("test_delta_merge")
+    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+    return spark
 
 
 @pytest.fixture
--- test_format_df.py
+++ test_format_df.py
@@ -9,12 +9,10 @@ from try_delta_merge import create_initial_delta_table, perform_delta_merge
 
 @pytest.fixture(scope="session")
 def spark():
-    return SparkSession.builder \
-        .appName("test") \
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-        .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0") \
-        .getOrCreate()
+    from delta import configure_spark_with_delta_pip
+    builder = SparkSession.builder.appName("test")
+    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+    return spark
 
 
 def test_format_df_adds_new_col(spark):
